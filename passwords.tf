e51a7edd897d8f36155157a44bdf46f653711e30
Vincent Balbarin <vbalbarin@microsoft.com>
2025-11-14 14:57:46 +0000

    Adding random password resource for hpcadmin

---
+++ passwords.tf
@@ -0,0 +1,26 @@
+# Random password generation for VM accounts using HashiCorp modules
+# This file generates strong, unique passwords for VM accounts and stores them in Azure Key Vault
+
+# Generate random password for domain controller admin account
+resource "random_password" "vm_hpcadmin" {
+  count = var.deploy_vm_cc ? 1 : 0
+
+  length  = 16
+  special = true
+  upper   = true
+  lower   = true
+  numeric = true
+
+  # Linux-safe
+  override_special = "!@#$%^&*()-_+="
+
+  # Exclude characters that can cause issues in PowerShell/Windows
+  # override_special = "!@#$%^&*()-_=+[]{}|;:,.<>?"
+
+  # Ensure we have at least one of each character type
+  min_upper   = 2
+  min_lower   = 2
+  min_numeric = 2
+  min_special = 2
+}
+
