c20bc8c831dd978067b96926450d8a6518253724
Vincent Balbarin <vbalbarin@microsoft.com>
2025-11-15 15:56:06 +0000

    Adding Storage Blob Data Reader role to umi over locker storage account

--- umi_locker.tf
+++ umi_locker.tf
@@ -14,3 +14,10 @@ module "avm-res-managedidentity-userassignedidentity" {
 output "userassigned_managed_identity_locker_resourceid" {
   value = module.avm-res-managedidentity-userassignedidentity.resource.id
 }
+
+resource "azurerm_role_assignment" "storage_blob_data_reader" {
+  scope                            = module.storage.resource_id
+  role_definition_name             = "Storage Blob Data Reader"
+  principal_id                     = module.avm-res-managedidentity-userassignedidentity.principal_id
+  skip_service_principal_aad_check = false
+}
